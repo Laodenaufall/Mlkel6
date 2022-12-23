2b7f0a8ee69bebd8ddf96712f75b96cc072d27a2
Laodenaufall <119912990+Laodenaufall@users.noreply.github.com>
2022-12-23 19:59:05 +0700

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -5,37 +5,15 @@ from sklearn.model_selection import train_test_split
 
 def run_ml_app():
     
-    ##st.write( """
-    ## Keterangan Data Yang Digunakan
-    ##1. GRE Score: Merupakan Score Test Untuk Masuk Program S2 (0 - 340) Bersifat Continous
-    ##2. TOEFL Score: Score Kemampuan TOEFL (0 - 120) Bersifat Continous
-    ##3. University Rating: Rating Universitas (0 - 5) Bersifat Ordinal
-    ##4. Kekuatan Surat Rekomendasi (0 - 5) Bersifat Ordinal
-    ##5. GPA Sewaktu Undergraduate (0 - 10) Bersifat Continous
-    ##6. Pengalaman Riset (0 : tidak ada, 1 : ada) Bersifat Nominal
+   
     
-    ##7. Peluang Diterima (0 - 1) Merupakan Dependent Variable
-    """)
-    
-    #st.write("""
-        ## Overview Data
-    #""")
+   
 
     myData = pd.read_csv('data.csv')
 
-    #st.dataframe(myData)
+   
 
-    #st.write("""
-        ## Deskripsi Data
-    #""")
-
-    #st.dataframe(myData.describe())
-
-    # Preproccessing Data
-
-    #st.write("""
-        ## Dilakukan Preprocessing Data dimana Fitur dan Labelnya akan Dipisah
-    #""")
+   
 
     # Memisahkan Label Dan Fitur 
     X = myData.iloc[:, 1:-1].values
@@ -43,8 +21,7 @@ def run_ml_app():
 
 
 
-    #st.write("## Input Data X",X)
-    #st.write("## Label Data y",y)
+    
 
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
